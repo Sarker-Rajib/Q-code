a351507ccbb821a027abc3364716733ad775b314
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-11 23:26:25 +0600

    chart added

--- src/component/Quizes/Quizes.js
+++ src/component/Quizes/Quizes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import QuizOptions from '../QuizOptions/QuizOptions';
 
 const Quizes = (quiz) => {
-   const { question, options } = quiz.quizes;
+   const { id, question, options } = quiz.quizes;
    // correctAnswer
    const questionSplit1 = question.split('<p>');
    const questionSplitted = questionSplit1[1].split('</p>');
 
    return (
       <div className="bg-info p-3 rounded mb-4 border border-primary text-white shadow">
-         <h6>{questionSplitted}</h6>
+         <h5>Question: {questionSplitted}</h5>
          <div className="row g-2 pt-3">
             {
                options.map((option, index) => <QuizOptions key={index} option={option} question={question}></QuizOptions>)
--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { Tooltip, XAxis, YAxis, BarChart, Bar } from "recharts";
 
 const Statistics = () => {
+   const info = useLoaderData();
+   const data = info.data;
+
    return (
-      <div>
+      <div className="py-3">
          <div className="container">
-            <p>hello</p>
+            <p className="pb-4">Statistics of Quix data in number</p>
+
+            <BarChart width={700} height={300} data={data}>
+               <Bar dataKey="total" fill="#8884d8" />
+               <Tooltip />
+               <XAxis dataKey="name" />
+               <YAxis />
+               <Tooltip />
+            </BarChart>
          </div>
       </div>
    );
