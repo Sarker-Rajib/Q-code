b6b0d147f3359332db232c540c6e5158c3da2e92
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-12 20:22:35 +0600

    footer builded

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+   return (
+      <div>
+         <div className="border border-info text-center mt-5 mb-2 shadow py-3 bg-light">
+            <h4>All rights reserved Q'Code Test</h4>
+         </div>
+      </div>
+   );
+};
+
+export default Footer;
